c816b6247da0493af3cc578df70e879f94ba9f8a
Sameer Khan <sameerkhanofficial9@gmail.com>
2020-08-17 14:39:58 +0300

    V2

--- segmentation.py
+++ segmentation.py
@@ -17,6 +17,8 @@ elif MODE is "MPI" :
     POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
 
 
+
+
 frame1 = cv2.imread("imgs/single1.jpg")
 #cv2.imshow("orignal",frame1)
 frame=cv2.resize(frame1, (640, 960)) 
